a6a5082c6baf2d1a9005aacd7ccadbdc5f8f1a89
Long Huynh <Longhuynh741@yahoo.com>
2020-11-16 00:28:04 -0500

    changed employee table component file

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
 
     return (
 <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a className="navbar-brand" href="/"> <h1 className="text-center"> NavBar </h1></a>
+  <a className="navbar-brand" href="/"> <h1 className="text-center"> Employee Directory </h1></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
