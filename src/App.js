5b394eaa6be08bd8b6f2348f0c0a8649c5013a1c
Long Huynh <Longhuynh741@yahoo.com>
2020-11-15 20:15:11 -0500

    created navbar and employee table from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import EmployeeTable from './components/EmployeeTable';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <EmployeeTable />
     </div>
   );
 }
